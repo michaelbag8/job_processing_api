074438f895060e80edcd90e9a70535f5375c32ae
michaelbag8 <michaelbag8@gmail.com>
2026-07-18 19:50:20 +0100

    Add:word count processor

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strings"
 )
 
 type Processor interface{
@@ -10,10 +12,17 @@ type Processor interface{
 
 type ReverseProcessor struct{}
 
-func (p ReverseProcessor) Process(ctx context.Context, payload string) (string,error){
+func (r ReverseProcessor) Process(ctx context.Context, payload string) (string,error){
 	runes := []rune(payload)
 	for r, l := 0, len(runes)-1; r < l; r, l = r+1, l-1{
 		runes[r], runes[l] = runes[l], runes[r]
 	}
 	return string(runes), nil
+}
+
+type WordCountProcessor struct{}
+
+func (w WordCountProcessor) Process(ctx context.Context, payload string) (string,error){
+	words := strings.Fields(payload)
+	return fmt.Sprintf("%d", len(words)),nil
 }
